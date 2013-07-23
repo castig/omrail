35cdd8b897351a6646095c498faa03cb04fe0f4e
Adam Sebolka <adamsebolka@gmail.com>
2013-07-22 23:49:14 -0400

    Broken Pin Format

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Omrail::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # In Production, :host: should be set to the actual host of your aplication
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Omrail::Application.routes.draw do
+  resources :pins
+
+
+  devise_for :users
+
   get 'about' => 'pages#about'
 
   root :to => 'pages#home'
